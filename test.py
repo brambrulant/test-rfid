846f0c7829d748ade3bde74f5d35859d4f7a7d6c
Bram Schabbink <73956679+brambrulant@users.noreply.github.com>
2024-07-02 15:37:00 +0200

    up

--- test.py
+++ test.py
@@ -1,24 +1,10 @@
 import serial
 
-rfid_serial_port = serial.Serial("/dev/ttyUSB1", 9600)
+ser = serial.Serial("/dev/ttyUSB0", 38400)
 
-id_num = []
-i = 0
-while True:
-    serial_data = rfid_serial_port.read()
-    data = serial_data.decode('utf-8')
-    i = i + 1
-    if i == 12:
-        i = 0
-        ID = "".join(map(str, id_num))
-        print(ID)
-        id_num = []
-    else:
-        id_num.append(data)
-    i = i + 1
-    if i == 12:
-        i = 0
-        ID = "".join(map(str, id_num))
-        print(ID)
-    else:
-        id_num.append(data)
+while(ser.is_open == True):
+    rfidtag = ''
+    incomingByte = ser.read(21)
+    print(incomingByte)
+    for i in incomingByte:
+        rfidtag = rfidtag + hex(i)
