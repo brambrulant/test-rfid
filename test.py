f28135ad1a1add72dcc8833c1ea87812d3861179
Bram Schabbink <73956679+brambrulant@users.noreply.github.com>
2024-07-04 14:29:37 +0200

    up

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import time
 import string
 import datetime
 import csv
+import sqlite3
 ####WARNING!!!! The RFID Module MUST be connected through the non power USB port####
 serial_port = '/dev/ttyUSB0' #this should be correct, but if not working use $ python -m serial.tools.miniterm
 ser = serial.Serial(port=serial_port,baudrate = 38400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
@@ -12,6 +13,21 @@ ser = serial.Serial(port=serial_port,baudrate = 38400,parity=serial.PARITY_NONE,
 Stall_Time = 10               #Time to consider that the tag has completely left the reader area
 Last_Tag = "initialise value" #just need a value that isn't an RFID tag
 Last_Time = time.time() - Stall_Time  #set the time value to effectively 0 (for use in loop later)
+
+# SQLite database setup
+db_name = 'rfid_tags.db'
+conn = sqlite3.connect(db_name)
+cursor = conn.cursor()
+
+# Create table if not exists
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tags (
+        tag TEXT PRIMARY KEY,
+        time_of_first_scan TEXT,
+        time_of_last_scan TEXT
+    )
+''')
+conn.commit()
 
 #start set_up_the_reader()
 def set_up_the_reader():
@@ -30,20 +46,16 @@ def set_up_the_reader():
 	#ser.write(b'\nN5,03\r')                 #Region 06: JP  916~921MH
 #end set_up_the_reader()
 
-#start write_to_csv()
-# Write to CSV
-def write_to_csv(RFID_Tag, RFID_Time):
-    data = [RFID_Tag.decode('utf-8', 'ignore'), RFID_Time.strftime("%m/%d/%Y, %H:%M:%S"), datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")]
-    with open('test.csv', 'a+', newline='') as read_file:
-        reader = csv.reader(read_file)
-        writer = csv.writer(read_file)
-        for row in reader:
-            if data[0] in row:
-                row = [row[0], row[1], RFID_Time.strftime("%m/%d/%Y, %H:%M:%S")]
-                writer.writerow()
-                return
-            else:
-                writer.writerow(data)
+# Insert or update tag in the database
+def insert_or_update_tag(tag, first_scan, last_scan):
+    cursor.execute('SELECT * FROM tags WHERE tag = ?', (tag,))
+    data = cursor.fetchone()
+    if data is None:
+        cursor.execute('INSERT INTO tags (tag, time_of_first_scan, time_of_last_scan) VALUES (?, ?, ?)',
+                       (tag, first_scan, last_scan))
+    else:
+        cursor.execute('UPDATE tags SET time_of_last_scan = ? WHERE tag = ?', (last_scan, tag))
+    conn.commit()
 
 #start send_command()
 def send_command():
@@ -71,5 +83,13 @@ while True:
 	RFID = read_buffer()
 	RFID_Tag = RFID[0]
 	RFID_Time = RFID[1]
-	if len(RFID_Tag) > 15:                               #this should be about 15 normally
-		write_to_csv(RFID_Tag, RFID_Time)
+	  
+	if len(RFID_Tag) > 15:  # This should be about 15 normally
+		tag_str = RFID_Tag.decode('utf-8', 'ignore')
+		first_scan = RFID_Time.strftime('%Y-%m-%d %H:%M:%S.%f')
+		last_scan = RFID_Time.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+		insert_or_update_tag(tag_str, first_scan, last_scan)
+
+		print(RFID_Tag)  # Only display the tag id part of the value
+		print(RFID_Time)
