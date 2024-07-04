b7c4b5aec4b13f3e3fdbe0d74995e044f3c997a4
Bram Schabbink <73956679+brambrulant@users.noreply.github.com>
2024-07-04 11:30:07 +0200

    up

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def set_up_the_reader():
 	print()
 	power_level = '-2'			 #Reader power level from -2 ~ 25dB
 	ser.write(b'\nN0,25\r')
-	ser.write(b'\nN1,25\r')
+	ser.write(b'\nN1,-2\r')
 
 	#set up the region - this is the frequency of operation - uncomment correct line
 	#ser.write(b'\nN5,03\r')                 #Region 01: US  902~928MHz
